34b22a0b4de73c36ab261cf66f209c64985a8db5
JamesWhite10 <max.migalin10@gmail.com>
2021-08-12 20:49:38 +0300

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+
+    return (
+        <div className="App">
+            <script src="./Films/films.js"/>
+        </div>
+    );
+}
+
+export default App;
